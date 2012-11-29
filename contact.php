1b199e77e2ccb07194082798ea74a47931986f0d
Elijah Breder <elibreder@gmail.com>
2012-11-29 12:18:25 -0500

    Changed the look of the contact forms

--- contact.php
+++ contact.php
@@ -43,81 +43,75 @@
 	</div> <!-- masthead -->
     
     
-	<div id="content" class="clearfix">
+		<div id="content" class="clearfix">
 		
-			<h2>Contact EZRunPC.com</h2>
-			<br />
-			
-			<?php
-				if ($_POST && isset($missing) && !empty($missing)) {
-			?>
-			
-			<p class="warning">Not all required fields have been filled in.</p>
-			
-			<?php 
-				}
-				else if ($_POST && !$mailsent) {
-			?>
-				
-			<p class="warning">Sorry, there was a problem sending your message. Please try again later.</p>
-			
-			<?php 
-				}
-				else if ($_POST && $mailsent) {
-			?>
-				
-			<p>Thank you, your message has been sent.
-			
-			<?php 
-				}
-			?>
-						
-			<div class="formWrapper">
-				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-				<p class="help">Send us an email to let us know how we can help you. Required fields are indicated by a <em>*</em>.</p>
-	      	<fieldset>
-	        	<legend>Send An Email</legend>
-		        <div>
-		          <label for="realname">Name <em>*</em></label>
-		          <input class="formField" type="text" id="realname" name="realname" size="30" required="required" />
-		        </div>
-			
-		        <div>
-		          <label for="email">Email address <em>*</em></label>
-		          <input class="formField" type="email" id="email" name="email" size="30" required="required" placeholder="name@example.com" />
-		        </div>
-		  
-			      <div>
-		          <label for="phone">Telephone</label>
-		          <input class="formField" type="text" id="phone" name="phone" size="30" />
-		        </div>
-		  
-			    	<div>
-		          <label for="message">Message <em>*</em></label>
-		      		<textarea class="formField" id="message" name="message" rows="15" cols="20" required="required"></textarea>
-		      	</div>
-						
-						<div id="hiddenCaptcha">
-							<input type="text" name="hiddenCaptcha" value="" />
-						</div>												
-		     	</fieldset>
+				<h2>Contact EZRunPC.com</h2>
+				<br />
 					
-					<div class="submit">
-							<input class="submitButton" type="submit" name="Send" value="Send" />
-					</div>
-				</form>				
-			</div> <!-- formWrapper -->
+				<?php
+						if ($_POST && isset($missing) && !empty($missing)) {
+				?>
+					
+				<p class="warning">Not all required fields have been filled in.</p>
+					
+				<?php 
+						}
+						else if ($_POST && !$mailsent) {
+				?>
+						
+				<p class="warning">Sorry, there was a problem sending your message. Please try again later.</p>
+					
+				<?php 
+						}
+						else if ($_POST && $mailsent) {
+				?>
+						
+				<p>Thank you, your message has been sent.
+					
+				<?php 
+						}
+				?>
+						
+				<div class="formWrapper">
+						<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+										<p class="help">Send us an email and let us know how we can help you. Required fields are indicated by a <em>*</em>.</p>
+										<div>
+												<label for="realname">Name <em>*</em></label>
+												<input class="formField" type="text" id="realname" name="realname" size="30" required="required" />
+										</div>
 			
-			<div class="phonenumWrapper">
-				<p class="help">Call us by phone to book an appointment.</p>
-				<fieldset id="phonefieldset">
-	        	<legend>Phone</legend>
-						<p class=phoneNumber>555-555-5555</p>
-				</fieldset>
-			</div>
+										<div>
+												<label for="email">Email address <em>*</em></label>
+												<input class="formField" type="email" id="email" name="email" size="30" required="required" placeholder="name@example.com" />
+										</div>
+				
+									<div>
+												<label for="phone">Telephone</label>
+												<input class="formField" type="text" id="phone" name="phone" size="30" />
+										</div>
+				
+										<div>
+												<label for="message">Message <em>*</em></label>
+												<textarea class="formField" id="message" name="message" rows="15" cols="20" required="required"></textarea>
+											</div>
+								
+										<div class="submit">
+										 <input class="submitButton" type="submit" name="Send" value="Send" />
+										</div>
+										
+										<div id="hiddenCaptcha">
+											<input type="text" name="hiddenCaptcha" value="" />
+										</div>																												
+						</form>				
+				</div> <!-- formWrapper -->
+					
+				<div class="phonenumWrapper">
+						<p class="help">Call us and book an appointment.</p>				
+						<p class="phoneNumber">555-555-5555</p>				
+				</div>
 			
 		</div> <!-- content -->      
-  </div> <!-- wrapper -->
+</div> <!-- wrapper -->
 
 </body>
 </html>
