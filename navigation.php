db5505a97c81ea0c6ffe245daf457b345d808aa3
Elijah Breder <elibreder@gmail.com>
2012-12-22 19:32:26 -0500

    Got rid of submenus
    - Services page now has lists of services rather than reling on a
    submenu
    - add images for the services list

--- navigation.php
+++ navigation.php
@@ -4,18 +4,19 @@ echo '
 <nav>
   <ul class="navbar ribbon-content">
   
-    <li class="homeMenu"><a href="index.php">Home</a></li>
-    <li class="aboutMenu"><a href="index.php">About Us</a></li>
-    <li class="servicesMenu"><a href="services.php">Services</a>                        
-       <!-- <ul>
-        <li><a href="services.php?page=general">General Computing</a></li>
-        <li><a href="services.php?page=maintenance">Maintenance</a></li>
-        <li><a href="services.php?page=security">Security</a></li>
-        <li><a href="services.php?page=networking">Networking</a></li>
-        <li><a href="services.php?page=backup">Data Backup</a></li>          
-      </ul> -->
-    </li>
-    <li class="contactMenu"><a href="contact.php">Contact</a></li>
+    <li id="homeMenu"><a href="index.php">Home</a></li>
+    <li id="aboutMenu"><a href="index.php">About Us</a></li>
+    <li id="servicesMenu"><a href="services.php">Services</a>
+    <!-- <li id="servicesMenu"><a href="services.php">Services</a>                        
+      <ul class="submenu">
+        <li><a href="serviceswithsubmenus.php?page=general">General Computing</a></li>
+        <li><a href="serviceswithsubmenus.php?page=maintenance">Maintenance</a></li>
+        <li><a href="serviceswithsubmenus.php?page=security">Security</a></li>
+        <li><a href="serviceswithsubmenus.php?page=networking">Networking</a></li>
+        <li><a href="serviceswithsubmenus.php?page=backup">Data Backup</a></li>          
+      </ul>
+    </li> -->
+    <li id="contactMenu"><a href="contact.php">Contact</a></li>
   </ul>
 </nav>
 </div> <!-- navContainer -->
--- services.php
+++ services.php
@@ -5,103 +5,71 @@
     <meta charset="UTF-8">
     <title>EZRunPC.com - In Home Computer Services - Services</title>
     <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="services.css">    
 </head>
 
 <body id="servicesPage">
 <div id="pageWrapper">    
-    <div id="masthead">
-        <div id="navContainer">
-            <div id="navigation">            
-                <?php require_once('navigation.php'); ?>
-            </div> <!-- navigation -->
-        </div> <!-- navContainer -->
-    </div> <!-- masthead -->
     
-    
-    <div id="content">
-<?php
-
-$page = 'default';
-
-// $_GET will be set when a menu item is selected in the navigation
-if (isset($_GET['page']))
-	$page = $_GET['page'];
-
-if (!empty($page) && !$page == '') {
-  if ($page == 'general') {
-    echo '<ul>
-            <li>General Computing
-                <ul>
-                    <li>help with installing and using software</li>
-                    <li>internet, email help</li>
-                    <li>CD/DVD burning</li>
-                    <li>setting up and installing devices (printers, scanners, hard drives, etc.)</li>
-                </ul>
-            </li>
-        </ul>';
-  }
-  else if ($page == 'maintenance') {
-    echo '<ul>
-            <li>Maintenance
-                <ul>
-                    <li>uninstall unused programs</li>
-                    <li>clean up registry</li>
-                    <li>start-up optimization</li>
-                    <li>hard drive defragmenting</li>
-                    <li>update device drivers, software, Windows</li>                        
-                </ul>
-            </li>
-           </ul>';
-  }
-  else if ($page == 'security') {
-    echo '<ul>
-            <li>Security
-                <ul>
-                    <li>remove spyware, malware, viruses</li>
-                    <li>install security program(s)</li>                    
-                </ul>
-            </li>
-          </ul>';
-  }
-  else if ($page == 'networking') {
-    echo '<ul>
-            <li>Networking
-                <ul>
-                    <li>set up router</li>
-                    <li>configure wireless network</li>
-                    <li>set up file and printer sharing with other computers</li>
-                    <li>integrate devices into home network:
-                        <ul>
-                            <li>game consoles, networked AV receivers, iPad/iPod, AppleTV, etc</li>
-                            <li>recover lost wireless network keys</li>
-                        </ul>
-                    </li>                    
-                </ul>
-            </li>
-          </ul>';
-  }
-  else if ($page == 'backup') {
-    echo '<ul>
-            <li>Data Backup
-                <ul>
-                    <li>Backup your precious data (photos, documets, music, videos, etc.)</li>
-                </ul>
-            </li>
-          </ul>';
-  }
-  else if ($page == 'default') {
-  	echo '<p>We offer a variety of services to help you get set up or solve computing issues.</p>';
-  }
-  
-  else {
-    echo "unknown page: $page.";
-  }
-}
-
-?>
-</div> <!-- content -->
+    <?php require_once('navigation.php'); ?>            
         
-  </div> <!-- wrapper -->
+    <div id="content">
+      
+      <h1>Our Services</h1>
+      
+      <ul>
+        <li id="general">General Computing
+            <ul>
+                <li>help with installing and using software</li>
+                <li>internet, email help</li>
+                <li>CD/DVD burning</li>
+                <li>setting up and installing devices (printers, scanners, hard drives, etc.)</li>
+            </ul>
+        </li>
+      </ul>
+      
+      <ul>
+        <li id="maintenace">Maintenance
+            <ul>
+                <li>uninstall programs you don't use</li>
+                <li>clean up the registry</li>
+                <li>start-up optimization</li>
+                <li>hard drive defragmenting</li>
+                <li>update device drivers, software, Windows</li>                        
+            </ul>
+        </li>
+      </ul>
+      
+      <ul>
+        <li id="security">Security
+          <ul>
+            <li>remove spyware, malware, viruses</li>
+            <li>install security program(s)</li>                    
+          </ul>
+        </li>
+      </ul>
 
+      <ul>
+        <li id="networking">Networking
+          <ul>
+            <li>set up router</li>
+            <li>configure wireless network</li>
+            <li>recover lost wireless network keys</li>
+            <li>set up file and printer sharing with other computers</li>
+            <li>integrate devices into home network (game consoles, AV receivers, iPad/iPod, AppleTV, etc.)</li>                    
+          </ul>
+        </li>
+      </ul>
+
+      <ul>
+        <li id="databackup">Data Backup
+            <ul>
+              <li>Backup your precious data (photos, documets, music, videos, etc.)</li>
+            </ul>
+        </li>
+      </ul>
+      
+    </div>
+  </div>
 </body>
 </html>
